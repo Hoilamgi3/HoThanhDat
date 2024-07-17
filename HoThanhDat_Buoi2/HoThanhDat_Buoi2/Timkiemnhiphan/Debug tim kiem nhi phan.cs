e2e5bea1f6baf64c7ee2f8546b50157a4f8e890e
HoThanhDat <hothanhdatsvhufibm@gmail.com>
2024-07-17 15:46:59 +0700

    PTTKTT

---
+++ HoThanhDat_Buoi2/HoThanhDat_Buoi2/Timkiemnhiphan/Debug tim kiem nhi phan.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoThanhDat_Buoi2_MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            int[] array = { 3, 9, 10, 27, 38, 43, 82 };
+            int search = 43;                  
+            Console.WriteLine("phan tu can tim: " + search);       
+            int index = BinarySearch(array, search);         
+            if (index != -1)
+            {
+                Console.WriteLine(search+ " duoc thay tai vi tri: " + index);
+            }
+            else
+            {
+                Console.WriteLine(search + " khong tim thay.");
+            }
+            Console.ReadLine();
+        }
+        public static int BinarySearch(int[] array, int search)
+        {
+            int left = 0;              
+            int right = array.Length - 1; 
+            while (left <= right)
+            {              
+                int mid = left + (right - left) / 2;
+                if (array[mid] == search)
+                {
+                    return mid;
+                }            
+                if (array[mid] < search)
+                {
+                    left = mid + 1;
+                }              
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1; 
+        }
+    }
+}
---
+++ HoThanhDat_Buoi2/HoThanhDat_Buoi2/Tinhtong2songuyenlon/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoThanhDat_Nhansonguyenlon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = "12345";
+            string num2 = "98765";
+            string result = Nhan(num1, num2);
+            Console.WriteLine("Result of {num1} * {num2} = {result}", num1, num2, result);
+            Console.ReadLine();
+        }
+        public static string Nhan(string num1, string num2)
+        {
+            char[] charNum1 = num1.ToCharArray();
+            char[] charNum2 = num2.ToCharArray();
+            Array.Reverse(charNum1);
+            Array.Reverse(charNum2);
+            int m = charNum1.Length;
+            int n = charNum2.Length;
+            int[] result = new int[m + n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result[i + j] += (charNum1[i] - '0') * (charNum2[j] - '0');
+                    result[i + j + 1] += result[i + j] / 10;
+                    result[i + j] %= 10;
+                }
+            }
+            int index = m + n - 1;
+            while (index > 0 && result[index] == 0)
+            {
+                index--;
+            }
+            string multiplyResult = "";
+            for (int k = index; k >= 0; k--)
+            {
+                multiplyResult += result[k];
+            }
+
+            return multiplyResult;
+        }
+       
+    }
+}
